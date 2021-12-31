7a32cc093b263e9f2756e4d6c305248db742d2d3
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2021-12-31 11:37:12 +0100

    chore: first commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
 
 export default function App() {
+  const progress = useRef(new Animated.Value(0.5)).current; // useSharedValue(0)
+  const scale = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    // withTiming, withSpring
+
+    // withRepeat
+    Animated.loop(
+      Animated.parallel([
+        Animated.sequence([
+          Animated.spring(progress, { toValue: 1, useNativeDriver: true }),
+          Animated.spring(progress, { toValue: 0.5, useNativeDriver: true }),
+        ]),
+        Animated.sequence([
+          Animated.spring(scale, { toValue: 2, useNativeDriver: true }),
+          Animated.spring(scale, { toValue: 1, useNativeDriver: true }),
+        ]),
+      ]),
+      { iterations: 3 }
+    ).start();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View
+        style={[
+          styles.square,
+          {
+            borderRadius: /* progress.value * SIZE / 2 */ progress.interpolate({
+              inputRange: [0.5, 1],
+              outputRange: [SIZE / 4, SIZE / 2],
+            }),
+            // borderRadius: Animated.multiply(progress, SIZE / 2),
+            opacity: progress,
+            transform: [
+              { scale },
+              {
+                rotate: /* progress.value * 2 * Math.PI */ progress.interpolate(
+                  {
+                    inputRange: [0.5, 1],
+                    outputRange: [Math.PI, 2 * Math.PI],
+                  }
+                ),
+              },
+              // {
+              //   rotate: Animated.multiply(progress, 2 * Math.PI),
+              // },
+            ],
+          },
+        ]}
+      />
     </View>
   );
 }
+
+const SIZE = 100.0;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +65,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: 'rgba(0,0,256,0.5)',
   },
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">
+    Introduction to Animated API ⚛
+</h1>
+
+<a href="https://youtu.be/PfC5Phrueww" target="_blank">
+    <img src="https://github.com/enzomanuelmangano/intro-to-animated/blob/main/.assets/introduction-to-animated.png" title="introduction-to-animated">
+</a>
+
+[Check out the video tutorial!](https://youtu.be/PfC5Phrueww)
+
+## Does this sound interesting?
+
+Then I suggest you check out my [YouTube](https://www.youtube.com/c/Reactiive) channel. I create React Native contents and continually update a series on Reanimated called ["Animate with Reanimated"](https://github.com/enzomanuelmangano/animate-with-reanimated).
